895552d160ff932fe0cbebc3b0ba99185313560e
juan <juan.mesaglio@gmail.com>
2018-04-22 22:18:50 -0300

    cambio para que se puedan consultar mas de un cliente

--- appCuentas/CuentasConsoleApp_EnCasaCompilaba/CuentasConsoleApp_EnCasaCompilaba/Program.cs
+++ appCuentas/CuentasConsoleApp_EnCasaCompilaba/CuentasConsoleApp_EnCasaCompilaba/Program.cs
@@ -15,42 +15,45 @@ namespace CuentasConsoleApp_EnCasaCompilaba
 			int cant = 000;
 
 			Console.WriteLine("Sistema de Conteo de Cuentas - En Casa Compilaba");
+            Console.WriteLine("En caso de querer salir en el dni escriba exit");
 			Console.WriteLine(string.Empty);
+            while (true) {
+                Console.Write("Ingrese el DNI del cliente: ");
+                string dni = Console.ReadLine();
+                if (dni == "exit") { break; }
+                Console.Write("Ingrese el saldo minimo: ");
+                string valor = Console.ReadLine();
 
-			Console.Write("Ingrese el DNI del cliente: ");
-			string dni = Console.ReadLine();
+                while (true)
+                {
+                    if (Double.TryParse(valor, out valorEnNumeros))
+                    {
+                        cant = cajero.contarCuentas(dni, valorEnNumeros);
+                        break;
+                    }
+                    Console.Write("No es un numero, intente otra vez: ");
+                    valor = Console.ReadLine();
+                }
 
-			Console.Write("Ingrese el saldo minimo: ");
-			string valor = Console.ReadLine();
+                Console.WriteLine(String.Empty);
+                Console.WriteLine(String.Empty);
 
-			while (true)
-			{
-				if (Double.TryParse(valor, out valorEnNumeros))
-				{
-					cant = cajero.contarCuentas(dni, valorEnNumeros);
-					break;
-				}
-				Console.Write("No es un numero, intente otra vez: ");
-				valor = Console.ReadLine();
-			}
+                if (cant != -1)
+                {
+                    Console.WriteLine("Cuentas del Cliente DNI {0} con Saldo superior a {1} = {2}", dni, valor, cant);
+                }
+                else
+                {
+                    Console.WriteLine("El Cliente no existe");
+                }
 
-			Console.WriteLine(String.Empty);
-			Console.WriteLine(String.Empty);
+                Console.WriteLine(String.Empty);
+                Console.WriteLine(String.Empty);
+                }
 
-			if (cant != -1)
-			{
-				Console.WriteLine("Cuentas del Cliente DNI {0} con Saldo superior a {1} = {2}", dni, valor, cant);
-			}
-			else
-			{
-				Console.WriteLine("El Cliente no existe");
-			}
+            Environment.Exit(0);
+            
 
-			Console.WriteLine(String.Empty);
-			Console.WriteLine(String.Empty);
-			Console.WriteLine("--Presione cualquier tecla para cerrar--");
-			Console.ReadKey();
-
-		}
+        }
 	}
 }
