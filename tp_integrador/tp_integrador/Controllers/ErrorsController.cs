da0e4ece78410e1b6380424d18599a5a8d8c4deb
NicolasGabrielPerez <nico__1993@hotmail.com>
2018-05-29 18:29:40 -0300

    error 404

---
+++ tp_integrador/tp_integrador/Controllers/ErrorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tp_integrador.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult Error404()
+        {
+            ActionResult result;
+
+        object model = Request.Url.PathAndQuery;
+
+        if (!Request.IsAjaxRequest())
+            result = View(model);
+        else
+            result = PartialView("_NotFound", model);
+
+        return result;
+    }
+}
+}
