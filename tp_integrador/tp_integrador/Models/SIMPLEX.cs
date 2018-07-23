c06b999552d371ab523373a6c9c1d0a51b4020eb
NicolasGabrielPerez <nico__1993@hotmail.com>
2018-07-23 18:01:39 -0300

    POST simplex

---
+++ tp_integrador/tp_integrador/Models/SIMPLEX.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace tp_integrador.Models
+{
+    public class SIMPLEX
+    {
+        private string API = "https://dds-simplexapi.herokuapp.com/consultar";
+
+        public string Simplex(string postData)
+        {
+            WebRequest request = WebRequest.Create(API);
+            request.Method = "POST";
+
+            postData = " ";
+
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+           // request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+
+    }
+}
