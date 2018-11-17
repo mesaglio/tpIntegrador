27cb33f159a8056ab642760c2e03f1d66cd7f841
juan <juan.mesaglio@gmail.com>
2018-11-17 12:35:01 -0300

    inicio de reportes y odm

---
+++ tp_integrador/tp_integrador/Models/ODM.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace tp_integrador.Models
+{
+    public class ODM
+    {
+        public void mapeo()
+        {
+            BsonClassMap.RegisterClassMap<Reporte>();
+        }
+        public IMongoDatabase conection()
+        {
+            var monguis = new MongoClient("mongodb://<disenio>:<disenio2018>@ds031108.mlab.com:31108/sgemdb");
+            return monguis.GetDatabase("sgemdb");
+        }
+        public void agregarReporte(IMongoDatabase data, string cliente,string anio,string mes, string consumo)
+        {
+            mapeo();
+            var reportes = data.GetCollection<Reporte>("userreportes");
+            var reporte = new Reporte(cliente, anio, mes, consumo);
+
+            reportes.InsertOne(reporte);
+        }
+        public void eliminarReporteDeClientePorAnioMes(IMongoDatabase data,string _cliente,string _anio,string _mes)
+        {
+            mapeo();
+            //filtro
+            var builder = Builders<Reporte>.Filter;
+            var filtro = builder.Eq("clienteID", _cliente) & builder.Eq("anio", _anio) & builder.Eq("mes", _mes);
+
+            var reportes = data.GetCollection<Reporte>("userreportes");
+
+            reportes.DeleteOne(filtro);
+            
+            
+        }
+
+    }
+}
