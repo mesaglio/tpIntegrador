637aacf8c08e3efa5df3a3bf5c5a4d2ad9fffe43
Mauro Conti <conti_mauro@hotmail.com>
2018-12-06 08:03:41 -0300

    fix conexion, agrego data a error

--- tp_integrador/tp_integrador/Controllers/ClienteController.cs
+++ tp_integrador/tp_integrador/Controllers/ClienteController.cs
@@ -242,7 +242,7 @@ namespace tp_integrador.Controllers
 				if (!SessionStateOK()) return View("Index");
 				if ((bool)Session["Admin"]) return PermisoDenegado();
 
-				Cliente uncliente = (Cliente)Session["Usuario"];
+				Cliente uncliente = null;//(Cliente)Session["Usuario"];
 				uncliente.CambiarAutoSimplex();
 
 				return View("Simplex", model: uncliente);
